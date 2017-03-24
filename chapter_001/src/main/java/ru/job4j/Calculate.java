7b49b8e147d7023ab33c330da982cebe4bbf01fc
jawer <hednight@gmail.com>
2017-03-23 20:58:37 -0400

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
 
+/**
+ * Class Calculate решение задачи части 1 урока 1.3
+ * @author Mikhail Vlasov
+ * @since 3.15.2017
+ * @version 1
+ */
 public class Calculate {
+	
+	/**
+	 * Конструктор, вывод строки в консоль
+	 * @param args массив строк
+	 * @return void	 
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world.");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Mikhail Vlasov (mailto:hednight@gmail.com)
+ * @version 1
+ * @since 3.15.2017
+ */ 
+package ru.job4j;
