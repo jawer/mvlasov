6678d5e9a4000b1ca0eaaee8687dcf28ba7b4ea7
jawer <hednight@gmail.com>
2017-04-09 00:23:37 -0400

    task 4412

--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -19,10 +19,10 @@ public class PaintTest {
 	 */
     @Test
     public void whenPiramidWithHeightTwoThenStringWithTwoRows() {
-        Paint paint = new Paint();
-        String result = paint.piramid(2);
-        String expected = String.format(" ^ %s^^^", System.getProperty("line.separator"));
-        assertThat(result, is(expected));
+		Paint paint = new Paint();
+		String result = paint.piramid(2);
+		String expected = String.format(" ^ %s^^^", System.getProperty("line.separator"));
+		assertThat(result, is(expected));
     }
 
 	/**.
@@ -31,8 +31,8 @@ public class PaintTest {
     @Test
     public void whenPiramidWithHeightThreeThenStringWithThreeRows() {
 		Paint paint = new Paint();
-        String result = paint.piramid(3);
-        String expected = String.format("  ^  %s ^^^ %s^^^^^", System.getProperty("line.separator"), System.getProperty("line.separator"));
-        assertThat(result, is(expected));
+		String result = paint.piramid(3);
+		String expected = String.format("  ^  %s ^^^ %s^^^^^", System.getProperty("line.separator"), System.getProperty("line.separator"));
+		assertThat(result, is(expected));
     }
 }
