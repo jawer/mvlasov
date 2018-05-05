557df95f9ef3e96d570da8368d8dfdc3888ebc0d
jawer <hednight@gmail.com>
2018-05-05 13:53:36 -0400

    Task 1005

--- chapter_005/src/test/java/ru/job4j/mapdiscovery/UserTest.java
+++ chapter_005/src/test/java/ru/job4j/mapdiscovery/UserTest.java
@@ -6,6 +6,8 @@ import static org.hamcrest.Matchers.is;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 /**.
  * Class UserTest tests User class.
@@ -26,5 +28,23 @@ public class UserTest {
         assertThat(user.getName(), is("Mike"));
         assertThat(user.getChildren(), is(2));
         assertThat(new SimpleDateFormat("yyyy-MM-dd").format(user.getBirthday().getTimeInMillis()), is("1990-12-31"));
+    }
+
+    /**.
+     * Tests when two users created without equals() and hashCode() overriding.
+     */
+    @Test
+    public void whenTwoUsersAddedToMapWithoutEqualsHashCodeOverridingThenCheckResult() {
+        Calendar dayOfBirth = Calendar.getInstance();
+        dayOfBirth.set(1990, 11, 31);
+        User user1 = new User("Mike", 2, dayOfBirth);
+        User user2 = new User("Mike", 2, dayOfBirth);
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, "user1");
+        map.put(user2, "user2");
+
+        for (Map.Entry<User, Object> entry : map.entrySet()) {
+            System.out.println("" + entry.getKey());
+        }
     }
 }
